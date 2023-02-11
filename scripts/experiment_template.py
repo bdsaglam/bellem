7b52e854b852760d5b26f059fb535dd65580c675
Barış Deniz Sağlam <bdsaglam@gmail.com>
2023-02-11 12:46:50 +0300

    more experiment utils

--- scripts/experiment_template.py
+++ scripts/experiment_template.py
@@ -22,9 +22,10 @@ from bellek.ml.vision import *
 from bellek.utils import *
 
 
-def run_experiment(config, wandb_run):
+def run_experiment(wandb_run):
+    config = Tree.from_flat_dict(wandb_run.config)
     print("Config")
-    print(wandb_run.config)
+    print(config)
 
     seed = config.get("seed")
     if seed is not None:
@@ -43,18 +44,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     with open(args.cfg) as f:
-        config = Tree(json.load(f))
-
-    # prepare config
-    if "device" not in config:
-        config["device"] = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    for k, v in config.flat().items():
-        if isinstance(k, str) and k.endswith("path"):
-            config.set(k, str(Path(v).resolve()))
+        config = prepare_config(Tree(json.load(f)))
 
     with context_chdir(make_experiment_dir()):
         wandb_config = config["wandb"]
-        with wandb.init(**wandb_config) as wandb_run:
-            wandb_run.config.update(config.flat())
-            run_experiment(config, wandb_run)
+        with wandb.init(config=config.flat(), **wandb_config) as wandb_run:
+            run_experiment(wandb_run)
