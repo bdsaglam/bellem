4a452442ba34afbfb9c8e47a384983803ba78320
Barış Deniz Sağlam <bdsaglam@gmail.com>
2023-02-11 11:21:13 +0300

    give up using delegates

--- bellek/ml/evaluation.py
+++ bellek/ml/evaluation.py
@@ -45,7 +45,6 @@ def find_binary_clf_threshold(targets, preds, metrics, *, threshold_range=None, 
 # %% ../../nbs/ml.evaluation.ipynb 7
 import pandas as pd
 from sklearn.metrics import classification_report
-from fastcore.meta import delegates
 
 def classification_summary(targets, preds, class_names, show=False, figsize=(16, 12)):
     label_indices = list(range(len(class_names)))
@@ -72,8 +71,7 @@ def classification_summary(targets, preds, class_names, show=False, figsize=(16,
         )
     return df
 
-@delegates(classification_summary, but=["targets", "preds", "class_names"])
-def evaluate_slmc(learn, dls=None, dl=None, class_names=None, **kwargs):
+def evaluate_slmc(learn, *, dls=None, dl=None, class_names=None, **kwargs):
     assert not (dls is None and dl is None)
     class_names = ifnone(class_names, dls.vocab)
     dl = ifnone(dl, dls.valid)
