1f3056bb1a1b0832d67403c8c077e91fd25fd3e5
Barış Deniz Sağlam <bdsaglam@gmail.com>
2023-02-28 13:37:09 +0300

    update cocoop script and configs

--- scripts/experiment/cocoop/sweep.sh
+++ scripts/experiment/cocoop/sweep.sh
@@ -6,7 +6,7 @@ PROJECT=$(jq -r '.wandb.project' config.json)
 wandb sweep --entity "${ENTITY}" --project "${PROJECT}" sweep-config.yaml 2>&1 | tee /tmp/capture.out 
 SWEEP_ID=$(cat /tmp/capture.out | grep -e "wandb: Created sweep with ID:" | awk -F':' '{print $NF}' | awk '{$1=$1;print}')
 
-wandb agent --count 2 "${ENTITY}/${PROJECT}/${SWEEP_ID}"
+wandb agent --count 20 "${ENTITY}/${PROJECT}/${SWEEP_ID}"
 
 wandb sweep --stop "${ENTITY}/${PROJECT}/${SWEEP_ID}"
 
