8b8a4a78023aa1619dad5b23874df9d65f9c527a
Barış Deniz Sağlam <bdsaglam@gmail.com>
2023-04-22 16:40:48 +0300

    change dir before downloading imagenet

--- scripts/imagenet/download-train.sh
+++ scripts/imagenet/download-train.sh
@@ -2,6 +2,8 @@
 
 set -o pipefail
 
+cd $1
+
 ## 1. Download the data
 
 # get ILSVRC2012_img_train.tar (about 138 GB). MD5: 1d675b47d978889d74fa0da5fadfb00e
@@ -16,7 +18,7 @@ tar -xvf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
 find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
 cd ..
 
-## 4. Delete corrupted image
+## 3. Delete corrupted image
 # there is one png under JPEG name. some readers fail on this image so need to remove it
 # this line is commented by default to avoid such unexpected behaviour
 rm train/n04266014/n04266014_10835.JPEG
--- scripts/imagenet/download-val.sh
+++ scripts/imagenet/download-val.sh
@@ -2,6 +2,8 @@
 
 set -o pipefail
 
+cd $1
+
 ## 1. Download the data
 
 # get ILSVRC2012_img_val.tar (about 6.3 GB). MD5: 29b22e2961454d5413ddabcf34fc5622
@@ -10,6 +12,6 @@ then
     wget --no-check-certificate https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar
 fi
 
-## 3. Extract the validation data and move images to subfolders:
+## 2. Extract the validation data and move images to subfolders:
 mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xvf ILSVRC2012_img_val.tar
 wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
