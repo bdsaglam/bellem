f8cef5c0395f11ddd01c553d4dc120adc20520da
Barış Deniz Sağlam <bdsaglam@gmail.com>
2023-11-25 15:14:05 +0300

    refactors

---
+++ scripts/kg-cons/merge.py
@@ -0,0 +1,33 @@
+import json
+from typing import Any, Dict, List, Tuple  # noqa: F401
+
+import torch
+from peft import AutoPeftModelForCausalLM
+
+from bellek.utils import NestedDict
+from bellek.logging import setup_logger
+
+log = setup_logger()
+
+
+def main(args):
+    with open(args.cfg) as f:
+        config = NestedDict.from_flat_dict(json.load(f))
+
+    model_id = config.at("hfhub.model_id")
+    model = AutoPeftModelForCausalLM.from_pretrained(
+        model_id,
+        low_cpu_mem_usage=True,
+        torch_dtype=torch.bfloat16,
+    )
+    model = model.merge_and_unload()
+    model.push_to_hub(f"{model_id}-merged")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cfg", default="./config.json")
+    args, _ = parser.parse_known_args()
+    main(args)
