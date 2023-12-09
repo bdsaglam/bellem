2c9ba75b03e8afe9b341627f7b6cb64aa6ce6fdc
Barış Deniz Sağlam <bdsaglam@gmail.com>
2023-12-09 16:28:42 +0300

    add peft utils

--- scripts/kg-cons/merge.py
+++ scripts/kg-cons/merge.py
@@ -1,32 +1,14 @@
 import json
 
-import torch
-from peft import AutoPeftModelForCausalLM
-from transformers import AutoTokenizer
-
-from bellek.logging import get_logger
+from bellek.ml.peft import merge_and_publish
 from bellek.utils import NestedDict
-
-log = get_logger(__name__)
 
 
 def main(args):
     with open(args.cfg) as f:
         config = NestedDict.from_flat_dict(json.load(f))
-
     model_id = config.at("hfhub.model_id")
-    model = AutoPeftModelForCausalLM.from_pretrained(
-        model_id,
-        torch_dtype=torch.float16,
-        device_map={"": 0},
-    )
-    tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
-
-    model = model.merge_and_unload()
-
-    merged_model_id = f"{model_id}-merged"
-    model.push_to_hub(merged_model_id)
-    tokenizer.push_to_hub(merged_model_id)
+    return merge_and_publish(model_id)
 
 
 if __name__ == "__main__":
--- scripts/tgi/run.sh
+++ scripts/tgi/run.sh
@@ -3,4 +3,5 @@ model=NousResearch/llama-2-7b-chat-hf
 volume="${HOME}/hf-tgi/data"
 mkdir $volume
 
-docker run --gpus all --shm-size 1g -p 8080:80 -v $volume:/data ghcr.io/huggingface/text-generation-inference:1.2 --model-id $model
+docker run --gpus all --shm-size 1g -p 8080:80 -v $volume:/data ghcr.io/huggingface/text-generation-inference:1.2 \
+    --model-id $model --trust-remote-code --quantize bitsandbytes-fp4 --dtype float16 
