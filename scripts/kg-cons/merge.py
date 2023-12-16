cccd3aa9dadc4c7e77ff4e05834e37676a9cebe4
Barış Deniz Sağlam <bdsaglam@gmail.com>
2023-12-16 23:53:22 +0300

    minor

--- scripts/kg-cons/merge.py
+++ scripts/kg-cons/merge.py
@@ -1,7 +1,5 @@
 import json
 
-import torch
-
 from bellek.ml.transformers import merge_adapters_and_publish
 from bellek.utils import NestedDict
 
@@ -10,7 +8,7 @@ def main(args):
     with open(args.cfg) as f:
         config = NestedDict.from_flat_dict(json.load(f))
     model_id = config.at("hfhub.model_id")
-    torch_dtype = config.at("pretrained_model.torch_dtype", torch.float16)
+    torch_dtype = config.at("pretrained_model.torch_dtype", "float16")
     return merge_adapters_and_publish(model_id, torch_dtype=torch_dtype)
 
 
