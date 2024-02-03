366a2b9ba0458f8a09c48769bb321df22445c341
Barış Deniz Sağlam <bdsaglam@gmail.com>
2024-02-03 13:25:44 +0300

    add train eval script

---
+++ scripts/kg-cons/train_eval.py
@@ -0,0 +1,42 @@
+import wandb
+from bellek.hf.transformers.experiment import evalu8, fine_tune, preprocess_config
+from bellek.jerx.eval import parse_triplet_strings
+from bellek.logging import get_logger
+from bellek.ml.experiment import main
+from bellek.utils import NestedDict, flatten_dict
+
+log = get_logger(__name__)
+
+
+def run_experiment(wandb_run):
+    # prepare config
+    config = preprocess_config(NestedDict.from_flat_dict(wandb_run.config))
+    wandb_run.config.update(flatten_dict(config), allow_val_change=True)
+
+    # fine-tuning
+    trainer = fine_tune(config)
+
+    # evaluation
+    config = NestedDict.from_flat_dict(wandb_run.config)
+    scores, eval_df = evalu8(
+        config,
+        tokenizer=trainer.tokenizer,
+        model=trainer.model,
+        output_parse_fn=parse_triplet_strings,
+    )
+    wandb_run.log(
+        {
+            **scores,
+            "evaluation-dataframe": wandb.Table(dataframe=eval_df.reset_index()),
+        }
+    )
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cfg", default="./config.json")
+    args, _ = parser.parse_known_args()
+
+    main(run_experiment, args)
