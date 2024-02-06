047ca14ed7958831a87bdeb0324070efe7bcbced
Barış Deniz Sağlam <bdsaglam@gmail.com>
2024-02-06 21:32:43 +0300

    add new script

---
+++ scripts/genesis-tgi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+model='bdsaglam/llama-2-7b-chat-jerx-mt-ss-peft-2024-02-04T00-14-15'
+
+VOLUME="${HOME}/.cache/huggingface/tgi"
+mkdir -p $VOLUME
+
+docker run --gpus all --shm-size 1g \
+    -p 8080:80 \
+    -v "${VOLUME}":/data \
+    ghcr.io/huggingface/text-generation-inference:1.4 \
+    --trust-remote-code \
+    --model-id $model --dtype float16
