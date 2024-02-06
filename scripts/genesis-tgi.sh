25e116487e95a7ee1a184927ef9096caa3369b6e
Barış Deniz Sağlam <bdsaglam@gmail.com>
2024-02-06 21:55:24 +0300

    update tgi script

--- scripts/genesis-tgi.sh
+++ scripts/genesis-tgi.sh
@@ -8,6 +8,6 @@ mkdir -p $VOLUME
 docker run --gpus all --shm-size 1g \
     -p 8080:80 \
     -v "${VOLUME}":/data \
-    ghcr.io/huggingface/text-generation-inference:1.4 \
+    ghcr.io/huggingface/text-generation-inference:1.3 \
     --trust-remote-code \
     --model-id $model --dtype float16
