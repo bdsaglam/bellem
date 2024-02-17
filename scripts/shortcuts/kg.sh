450184749bb992e6d046ebe68eed7a133fa9916e
Barış Deniz Sağlam <bdsaglam@gmail.com>
2024-02-17 11:24:28 +0300

    improve inspection notebook and scripts

---
+++ scripts/shortcuts/kg.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+open "./data/generated/musique-kg-llm/train/knowledge-graphs/${1}/kuzu-network.html"
---
+++ scripts/shortcuts/phoenix_qna.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python ./scripts/launch_phoenix.py "./data/generated/musique-kg-llm/train/question-answering/${1}/traces.jsonl"
