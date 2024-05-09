43e41fc7e954ec589a8c711d950bf82c9fe4a98e
Barış Deniz Sağlam <bdsaglam@gmail.com>
2024-05-09 21:17:30 +0300

    upload parquet file to wandb instead of table

--- scripts/kg-cons/eval.py
+++ scripts/kg-cons/eval.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import wandb
 
 from bellek.hf.transformers.experiment import evaluate_, preprocess_config
@@ -10,6 +11,15 @@ from bellek.utils import NestedDict
 log = get_logger(__name__)
 
 
+def log_evaluation_data(wandb_run, df: pd.DataFrame):
+    name = "evaluation-dataframe"
+    artifact = wandb.Artifact(name=name, type="dataset")
+    filename = name + ".parquet"
+    df.to_parquet(filename)
+    artifact.add_file(local_path=filename)
+    wandb_run.log_artifact(artifact)
+
+
 def run_experiment(wandb_run):
     config = NestedDict.from_flat_dict(wandb_run.config)
     if not wandb.run.resumed:
@@ -20,12 +30,8 @@ def run_experiment(wandb_run):
         metric_kwargs={"eq_fn": lambda a, b: fuzzy_match(a, b, threshold=0.8)},
         output_parse_fn=parse_triplet_strings,
     )
-    wandb_run.log(
-        {
-            **scores,
-            "evaluation-dataframe": wandb.Table(dataframe=eval_df.reset_index()),
-        }
-    )
+    wandb_run.log(scores)
+    log_evaluation_data(wandb_run, eval_df)
 
 
 if __name__ == "__main__":
