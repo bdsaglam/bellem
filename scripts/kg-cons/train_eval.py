1ed7d5ac408c50d4c4e6583633c6dd4ac94e487b
Barış Deniz Sağlam <bdsaglam@gmail.com>
2024-05-10 19:37:20 +0300

    update script

--- scripts/kg-cons/train_eval.py
+++ scripts/kg-cons/train_eval.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import wandb
+
 from bellek.hf.transformers.experiment import evaluate_, fine_tune, preprocess_config
 from bellek.jerx.eval import parse_triplet_strings
 from bellek.logging import get_logger
@@ -7,6 +9,15 @@ from bellek.text.utils import fuzzy_match
 from bellek.utils import NestedDict, flatten_dict
 
 log = get_logger(__name__)
+
+
+def log_evaluation_data(wandb_run, df: pd.DataFrame):
+    name = "evaluation-dataframe"
+    artifact = wandb.Artifact(name=name, type="dataset")
+    filename = name + ".parquet"
+    df.to_parquet(filename)
+    artifact.add_file(local_path=filename)
+    wandb_run.log_artifact(artifact)
 
 
 def run_experiment(wandb_run):
@@ -26,12 +37,8 @@ def run_experiment(wandb_run):
         metric_kwargs={"eq_fn": lambda a, b: fuzzy_match(a, b, threshold=0.8)},
         output_parse_fn=parse_triplet_strings,
     )
-    wandb_run.log(
-        {
-            **scores,
-            "evaluation-dataframe": wandb.Table(dataframe=eval_df.reset_index()),
-        }
-    )
+    wandb_run.log(scores)
+    log_evaluation_data(wandb_run, eval_df)
 
 
 if __name__ == "__main__":
