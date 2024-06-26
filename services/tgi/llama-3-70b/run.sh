341cdb0849be383b9d2e01b646ddd9c34c105495
Barış Deniz Sağlam <bdsaglam@gmail.com>
2024-06-26 11:51:02 +0300

    add service for llama-3 70b inference

--- services/tgi/llama-3-70b/run.sh
+++ services/tgi/llama-3-70b/run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+HUGGING_FACE_HUB_TOKEN="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"
+
 MODEL="meta-llama/Meta-Llama-3-70B-Instruct"
 
 VOLUME="${HOME}/.cache/huggingface/tgi"
 mkdir -p $VOLUME
 
-IMAGE=bdsaglam/text-generation-inference:latest
+IMAGE=ghcr.io/huggingface/text-generation-inference:2.0.4
 
-docker run \
-    --gpus "2,3" \
-    --shm-size 1g \
+CUDA_VISIBLE_DEVICES=0,1 docker run \
+    --gpus all \
+    --shm-size 64g \
     -p 8080:80 \
     -v "${VOLUME}":/data \
     -e HUGGING_FACE_HUB_TOKEN=$HUGGING_FACE_HUB_TOKEN \
+    -e CUDA_VISIBLE_DEVICES=0,1 \
     $IMAGE \
     --trust-remote-code --model-id $MODEL --dtype bfloat16 --quantize bitsandbytes-nf4 --num-shard 2 
