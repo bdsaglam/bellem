aa003c70fc00847b057fb1f5c562e1a8be9039b7
Barış Deniz Sağlam <bdsaglam@gmail.com>
2024-07-25 19:35:39 +0300

    more ablation

--- services/tgi/llama-3-8b/run.sh
+++ services/tgi/llama-3-8b/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 HUGGING_FACE_HUB_TOKEN="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"
 
-MODEL="meta-llama/Meta-Llama-3-70B-Instruct"
+MODEL="meta-llama/Meta-Llama-3-8B-Instruct"
 
 VOLUME="${HOME}/.cache/huggingface/tgi"
 mkdir -p $VOLUME
@@ -11,9 +11,9 @@ IMAGE=ghcr.io/huggingface/text-generation-inference:2.0.4
 docker run \
     --gpus all \
     --shm-size 64g \
-    -p 8080:80 \
+    -p 8081:80 \
     -v "${VOLUME}":/data \
     -e HUGGING_FACE_HUB_TOKEN=$HUGGING_FACE_HUB_TOKEN \
-    -e CUDA_VISIBLE_DEVICES=1,2 \
+    -e CUDA_VISIBLE_DEVICES=0 \
     $IMAGE \
-    --trust-remote-code --model-id $MODEL --dtype bfloat16 --quantize bitsandbytes-nf4 --num-shard 2 --max-input-tokens 7168 --max-total-tokens 8192
+    --trust-remote-code --model-id $MODEL --dtype bfloat16 --quantize bitsandbytes-nf4 --max-input-tokens 7168 --max-total-tokens 8192
