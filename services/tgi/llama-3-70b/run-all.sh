57e30d736adb2dc5c5b8bd28c981eb0eb7f9643a
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-01-22 23:08:04 +0300

    litellm + tgi optims

--- services/tgi/llama-3-70b/run-all.sh
+++ services/tgi/llama-3-70b/run-all.sh
@@ -6,18 +6,17 @@ MODEL="meta-llama/Meta-Llama-3-70B-Instruct"
 VOLUME="${HOME}/.cache/huggingface/tgi"
 mkdir -p $VOLUME
 
-IMAGE=ghcr.io/huggingface/text-generation-inference:2.3.0
+IMAGE=ghcr.io/huggingface/text-generation-inference:3.0.1
 
 docker run \
     --gpus all \
-    --shm-size 64g \
     -p 8080:80 \
     -v "${VOLUME}":/data \
     -e HUGGING_FACE_HUB_TOKEN=$HUGGING_FACE_HUB_TOKEN \
+    -e CUDA_VISIBLE_DEVICES=0,1,2,3 \
     $IMAGE \
     --trust-remote-code \
     --model-id $MODEL \
     --dtype bfloat16 \
-    --num-shard 4 --sharded true \
     --max-input-tokens 7168 \
     --max-total-tokens 8192
